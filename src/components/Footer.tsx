a07c6d44300718d459be90ad726264f87f5f1eb1
이광복 <galeb76@gmail.com>
2026-02-19 23:56:06 +0900

    Add Facebook and Instagram links to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,17 @@
 export default function Footer() {
     return (
-        <footer>
-            <div className="container">
-                <div className="footer-declaration">
-                    함께 걷는 믿음,<br />
-                    함께 자라는 교회
-                </div>
-                <p className="footer-copy">© 2016 Raon Donghang Church</p>
+        <div className="footer-content">
+            <div className="footer-declaration">
+                함께 걷는 믿음,<br />
+                함께 자라는 교회
             </div>
-        </footer>
+            <div className="footer-social">
+                <a href="https://www.facebook.com/raondonghang" target="_blank" rel="noopener noreferrer">Facebook</a>
+                <span className="separator">|</span>
+                <a href="https://www.instagram.com/raon_donghang/" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </div>
+            <p className="footer-copy">© 2016 Raon Donghang Church</p>
+        </div>
+        </footer >
     );
 }
